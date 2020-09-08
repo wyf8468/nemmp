15d16a16b477b61dd1d1422912ddcf2da4192a43
wyf8468 <1659246239@qq.com>
2020-09-08 11:36:19 +0800

    王玉凤：发送普通短信代码更新

--- nmmp_manage/cases/test_2_sendNorMsg.py
+++ nmmp_manage/cases/test_2_sendNorMsg.py
@@ -20,28 +20,33 @@ class TestLogin(unittest.TestCase):
 
 
     @classmethod
-    def setUpClass(self):
-        self.driver = webdriver.Chrome()
-        self.driver.maximize_window()
-        self.driver.get(web_login_url)
-        self.driver.implicitly_wait(5)  # 隐式等待，5秒钟内只要找到了元素就开始执行，5秒钟后未找到，就超时
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome()
+        cls.driver.maximize_window()
+        cls.driver.get(web_login_url)
+        cls.driver.implicitly_wait(5)  # 隐式等待，5秒钟内只要找到了元素就开始执行，5秒钟后未找到，就超时
+        # 先登录
+        comm_login(cls.driver).Loginfor(success_data['user'], success_data['pwd'], "1111")
 
     # 刷新一下当前页面
     def tearDown(self):
         self.driver.refresh()
 
     # 正常用例
-    def test_2_sendNor_success(self):
+    def test_sendNorMsg_2_success(self):
+        comm_frame(self.driver).Frame('mainFrame_26')  # 获取iframe
         logging.info("*********发送普通短信用例：正常场景-提交成功*********")
         SendMsgPage(self.driver).send_normal_msg(msgDatas.success_data["phone"], msgDatas.success_data["content"])
-        # 断言：首页 -【今日事务】这个元素存在
-        self.assertTrue(HomePage(self.driver).check_login_ele_exists())
+        self.driver.switch_to.default_content()  # 释放iframe
+        time.sleep(3)
+        SendMsgPage(self.driver).send_suceedPop()
+        time.sleep(3)
 
     # 异常用例
     @ddt.data(*msgDatas.wrong_datas)
-    def test_2_sendNor_filed(self, data):
-        # 先登录
-        comm_login(self.driver).Loginfor(success_data['user'], success_data['pwd'], "1111", 'mainFrame_26')
+    def test_sendNorMsg_1_error(self, data):
+        # 获取iframe
+        comm_frame(self.driver).Frame('mainFrame_26')
         # 选择菜单到发送普通短信页面
         MenuUtils(self.driver).menu_tab('发送普通短信')
         time.sleep(2)
@@ -50,15 +55,15 @@ class TestLogin(unittest.TestCase):
         time.sleep(2)
         logging.info("*********发送普通短信用例：异常场景-提交失败*********")
         SendMsgPage(self.driver).send_normal_msg(data["phone"], data["content"])
+        self.driver.switch_to.default_content()  # 释放iframe
         time.sleep(2)
         # 断言：判断提示信息是否一致
         self.assertEqual(data["check"], SendMsgPage(self.driver).get_errorMsg())
 
-
     @classmethod
-    def tearDownClass(self):
+    def tearDownClass(cls):
         # pass
-        self.driver.close()
+        cls.driver.close()
 
     if __name__ == "__main__":
         unittest.main(verbosity=2)
