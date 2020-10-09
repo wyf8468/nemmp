dc3d26ed589cd3cf5f56ef14d1247f0fc7a3b913
wyf8468 <1659246239@qq.com>
2020-10-09 10:49:49 +0800

    王玉凤：优化发送普通短信结果验证代码更新

--- nmmp_manage/cases/test_2_sendGeneral_msg.py
+++ nmmp_manage/cases/test_2_sendGeneral_msg.py
@@ -12,9 +12,9 @@ import ddt
 from nmmp_manage.pages.logic.send_msg.send_generalMsg_page import SendMsgPage
 from nmmp_manage.common.menuUtils import *
 from nmmp_manage.common.comm_login import *
-from nmmp_manage.common.comm_frame import *
 from nmmp_manage.pages.datas.login_datas import *
-
+from nmmp_manage.common.getLists import *
+from nmmp_manage.common.connectMysql import *
 
 @ddt.ddt
 class TestLogin(unittest.TestCase):
@@ -34,17 +34,54 @@ class TestLogin(unittest.TestCase):
 
     # 正常用例
     def test_sendGeneral_2_success(self):
+        temp = True
         comm_frame(self.driver).Frame('mainFrame_26')  # 获取iframe
         logging.info("*********发送普通短信用例：正常场景-立即发送提交成功*********")
         SendMsgPage(self.driver).send_normal_msg(msgDatas.success_data["phone"], msgDatas.success_data["content"])
         SendMsgPage(self.driver).send_immediately()  # 立即发送
         SendMsgPage(self.driver).send_submit()  # 提交发送
         self.driver.switch_to.default_content()  # 释放iframe
-        time.sleep(4)
-        SendMsgPage(self.driver).send_suceedPop()  # 点击弹窗上的确认按钮
         time.sleep(2)
+        SendMsgPage(self.driver).send_suceedPop()  # 点击弹窗上的确认按钮
+        # SendMsgPage(self.driver).send_check()
+        time.sleep(2)
+        # 获取列表返回值id
+        getDataId = getList(self.driver).get_list('mainFrame_28', '//*[@id="table_content"]', 'tr', 'dataid')
+        time.sleep(8)
+        SendMsgPage(self.driver).send_refresh()
+        # 获取短信审核箱中提交过来的审核状态
+        msg_trone = self.driver.find_element_by_xpath('//*[@id="table_content"]/tbody/tr[1]/td[6]/span')
+        print(msg_trone.text)
+        # 断言：判断结果信息是否一致
+        self.assertEqual(msgDatas.success_data["checkText"], msg_trone.text)
+        if msg_trone.text == msgDatas.success_data["checkText"]:
+            self.driver.switch_to.default_content()  # 释放iframe
+            time.sleep(1)
+            MenuUtils(self.driver).menu_tab('li', '已发短信')
+            time.sleep(2)
+            comm_frame(self.driver).Frame('mainFrame_30')  # 获取iframe
+            # 连接数据库循环判断已发短信是否有对应id
+            Arr = connectMysql(self.driver).connect_mysql('nemmp_sms_zk', "SELECT * FROM `sms_task_contact_202010`", int(getDataId), 0)
+            for taskId in Arr:
+                msg_alreadyTr = self.driver.find_element_by_css_selector('[dataid="'+str(taskId)+'"]')  # 通过dataid属性定位元素
+                # print(msg_alreadyTr.text)
+                msg_text = msg_alreadyTr.find_element_by_name('sendStatus')
+                msg_remark = msg_alreadyTr.find_element_by_name('statusCodeCh')
+                # print(msg_text.text)
+                # print('代码注释：' + msg_remark.text)
+                time.sleep(1)
+                if msgDatas.success_data["codeText"] != msg_text.text:
+                    print(msg_alreadyTr.text)
+                    print(msg_text.text)
+                    print('代码注释：' + msg_remark.text)
+                    temp = False
+                    break
+        self.assertTrue(temp)
+
+
 
     # 异常用例
+    @unittest.skip('普通短信下发用例无条件跳过')
     @ddt.data(*msgDatas.wrong_datas)
     def test_sendGeneral_1_error(self, data):
         # 获取iframe
